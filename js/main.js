5e1334e92cd58c23a2723396174c7b2b5bd95f1b
DrumPire <expresproject@gmail.com>
2022-04-24 16:47:47 +0300

    added timer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {accordeon} from './accordeon.js';
 import {modal} from './modal.js';
 import {smoothScroll} from './smoothScroll.js';
+import {timer} from './timer.js';
 
 accordeon();
 modal();
 smoothScroll();
+timer();
